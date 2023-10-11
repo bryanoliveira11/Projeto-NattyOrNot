574b468dfb22e9517557b37cb95c9d9c9718f483
bryan <bryan.oliveira12345@gmail.com>
2023-10-11 11:50:45 -0300

    domain

--- NattyOrNot/settings.py
+++ NattyOrNot/settings.py
@@ -33,10 +33,10 @@ SECRET_KEY = environ.get('SECRET_KEY', 'INSECURE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if environ.get('DEBUG') == '0' else True
 
-ALLOWED_HOSTS: List = [environ.get('RECAPTCHA_DOMAIN', '')]
+ALLOWED_HOSTS: List = [environ.get('RECAPTCHA_DOMAIN', 'www.nattyornot.online')]
 
 CSRF_TRUSTED_ORIGINS: List = [environ.get(
-    'CSRF_TRUSTED_ORIGINS', '')
+    'CSRF_TRUSTED_ORIGINS', 'https://www.nattyornot.online')
 ]
 
 # Application definition
