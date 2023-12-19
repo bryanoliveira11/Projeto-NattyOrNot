aa9db5ec60a405c39521ed5edf004f06691c98b4
bryan <bryan.oliveira12345@gmail.com>
2023-12-19 18:38:37 -0300

    ajuste e correção de bug na lógica da páginação

--- utils/pagination.py
+++ utils/pagination.py
@@ -8,24 +8,12 @@ from django.core.paginator import Paginator
 # s.punctuation, k=64)))"
 
 
-def make_pagination_range(
-    page_range,
-    qty_pages,
-    current_page,
-):
+def make_pagination_range(page_range, qty_pages, current_page):
     middle_range = math.ceil(qty_pages / 2)
-    start_range = current_page - middle_range
-    stop_range = current_page + middle_range
+    start_range = max(0, current_page - middle_range)
+    stop_range = min(current_page + middle_range, len(page_range))
+
     total_pages = len(page_range)
-
-    start_range_offset = abs(start_range) if start_range < 0 else 0
-
-    if start_range < 0:
-        start_range = 0
-        stop_range += start_range_offset
-
-    if stop_range >= total_pages:
-        start_range = start_range - abs(total_pages - stop_range)
 
     pagination = page_range[start_range:stop_range]
     return {
@@ -36,7 +24,7 @@ def make_pagination_range(
         'total_pages': total_pages,
         'start_range': start_range,
         'stop_range': stop_range,
-        'first_page_out_of_range': current_page > middle_range,
+        'first_page_out_of_range': start_range > 0,
         'last_page_out_of_range': stop_range < total_pages,
     }
 
