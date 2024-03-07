b25b66e01aa3f5e380155f52df56a4b15d78e14a
bryan <bryan.oliveira12345@gmail.com>
2024-03-07 12:33:14 -0300

    Padronização das urls do site

--- NattyOrNot/urls.py
+++ NattyOrNot/urls.py
@@ -24,8 +24,8 @@ handler404 = 'training.views.error404.error_404'
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
-    path('', include('training.urls')),
-    path('', include('users.urls')),
+    path('home', include('training.urls')),
+    path('user', include('users.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include("debug_toolbar.urls")),
 ]
--- users/urls.py
+++ users/urls.py
@@ -12,114 +12,114 @@ user_api_v1_router.register(
 
 urlpatterns = [
     # user /
-    path('user/register/', views.UserRegisterView.as_view(), name='register'),
-    path('user/login/', views.UserLoginView.as_view(), name='login'),
-    path('user/logout/', views.UserLogoutView.as_view(), name='logout'),
+    path('/register/', views.UserRegisterView.as_view(), name='register'),
+    path('/login/', views.UserLoginView.as_view(), name='login'),
+    path('/logout/', views.UserLogoutView.as_view(), name='logout'),
     # user profile urls
     path(
-        'user/<str:username>/profile/',
+        '/<str:username>/profile/',
         views.UserProfileDetailClassView.as_view(),
         name='user_profile'
     ),
     path(
-        'user/<str:username>/profile/change/password/',
+        '/<str:username>/profile/change/password/',
         views.UserProfileChangePassword.as_view(),
         name='user_profile_change_password'
     ),
     # notifications delete path
     path(
-        'user/notification/<int:id>/delete/',
+        '/notification/<int:id>/delete/',
         views.UserNotificationDeleteSingle.as_view(),
         name='notification_delete_single'
     ),
     path(
-        'user/notifications/delete/',
+        '/notifications/delete/',
         views.UserNotificationsDeleteClassView.as_view(),
         name='notifications_delete'
     ),
     # user workouts urls
     path(
-        'user/workouts/',
+        '/workouts/',
         views.UserWorkoutsPageClassView.as_view(),
         name='user_workouts'
     ),
     path(
-        'user/workouts/search/',
+        '/workouts/search/',
         views.UserWorkoutsPageSearchClassView.as_view(),
         name='user_workouts_search'
     ),
     path(
-        'user/workout/<int:id>/detail/',
+        '/workout/<int:id>/detail/',
         views.UserWorkoutsPageDetailClassView.as_view(),
         name='user_workout_detail'
     ),
     path(
-        'user/workout/create/',
+        '/workout/create/',
         views.UserWorkoutClassView.as_view(),
         name='user_workout_create'
     ),
     path(
-        'user/workout/<int:id>/edit/',
+        '/workout/<int:id>/edit/',
         views.UserWorkoutClassView.as_view(),
         name='user_workout_edit'
     ),
     # delete workout
     path(
-        'user/workout/<int:id>/delete/',
+        '/workout/<int:id>/delete/',
         views.UserWorkoutDeleteClassView.as_view(),
         name='user_workout_delete'
     ),
     # share workout
     path(
-        'user/workout/<int:id>/share/',
+        '/workout/<int:id>/share/',
         views.UserWorkoutShareClassView.as_view(),
         name='user_workout_share'
     ),
     path(
-        'user/workout/<int:id>/unshare/',
+        '/workout/<int:id>/unshare/',
         views.UserWorkoutUnshareClassView.as_view(),
         name='user_workout_unshare'
     ),
     # workout filters
     path(
-        'user/workouts/is_shared=<str:is_shared>/',
+        '/workouts/is_shared=<str:is_shared>/',
         views.UserWorkoutsIsSharedFilterClassView.as_view(),
         name='user_workout_is_shared'
     ),
     # user dashboard urls
     path(
-        'dashboard/exercises/',
+        '/dashboard/exercises/',
         views.DashboardUserClassView.as_view(),
         name='user_dashboard'
     ),
     path(
-        'dashboard/exercises/search/',
+        '/dashboard/exercises/search/',
         views.DashboardSearchClassView.as_view(),
         name='user_dashboard_search'
     ),
     path(
-        'dashboard/exercises/shared=<str:shared_status>/',
+        '/dashboard/exercises/shared=<str:shared_status>/',
         views.DashboardSharedStatusFilterClassView.as_view(),
         name='user_dashboard_shared_filter'
     ),
     path(
-        'dashboard/exercises/category/<int:id>/',
+        '/dashboard/exercises/category/<int:id>/',
         views.DashboardUserCategoryClassView.as_view(),
         name='user_dashboard_category'
     ),
     # delete exercise
     path(
-        'dashboard/exercise/<int:id>/delete/',
+        '/dashboard/exercise/<int:id>/delete/',
         views.UserExerciseDeleteClassView.as_view(),
         name='user_exercise_delete'
     ),
     path(
-        'dashboard/exercise/create/',
+        '/dashboard/exercise/create/',
         views.DashboardExerciseClassView.as_view(),
         name='create_exercise'
     ),
     path(
-        'dashboard/exercise/<int:id>/edit/',
+        '/dashboard/exercise/<int:id>/edit/',
         views.DashboardExerciseClassView.as_view(),
         name='edit_exercise'
     ),
